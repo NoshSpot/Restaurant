a2fe15a61846211d919691a98a4d9766c96c00c1
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-20 09:42:01 -0700

    Add reviews controller

--- app/js/reviews/reviews.controller.js
+++ app/js/reviews/reviews.controller.js
@@ -5,11 +5,22 @@
         .module('app')
         .controller('RestaurantReviewsController', RestaurantReviewsController);
 
-    RestaurantReviewsController.$inject = [];
+    RestaurantReviewsController.$inject = ['ReviewFactory', '$stateParams'];
 
     /* @ngInject */
-    function RestaurantReviewsController() {
+    function RestaurantReviewsController(ReviewFactory, $stateParams) {
         var vm = this;
         
+        ////////////////
+        activate();
+        ////////////////
+
+        function activate() {
+            ReviewFactory.getAll().then(
+                function(data) {
+                    vm.reviews = data;
+                }
+            );
+        }
     }
 })();
