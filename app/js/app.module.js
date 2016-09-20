1385b170391fdd273ae67d4bd35b4442d5f0e60a
CoLagon <colelogan19@yahoo.com>
2016-09-19 21:25:24 -0700

    module not complete

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -2,7 +2,38 @@
     'use strict';
 
     angular
-        .module('app', [
-            'ui.router'
-        ]);
+        .module('app', ['ui.router', 'toastr']);
+        .value('apiUrl', '')
+        .config(appConfig);
+
+        appConfig.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+        function appConfig($urlRouterProvider, $stateProvider) {
+
+        	$urlRouterProvider.otherwise('/login');
+
+        	$stateProvider
+        	.state('login', {
+        		url: '/login',
+        		controller: 'LoginController as login',
+        		templateUrl: 'js/login/login.html'
+        	})
+//////////////////////////////////////////////////////////////////////
+			
+				.state('orders', {
+					url: '/orders',
+					controller: 'OrdersDetailController as orderDetail',
+					templateUrl: 'js/orders/orders.html'
+				})
+				.state('menu', {
+					url: '/menu',
+					controller:'MenuDetailController as menuDetail',
+					templateUrl: 'js/menu/menu.html'
+				})
+				.state('reviews', {
+					url: '/reviews',
+					controller: 'ReviewsDetailController as reviewDetail',
+					templateUrl: 'js/reviews/reviews.html'
+			});
+        }
 })();
