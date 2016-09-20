0bcb8725c9fca2ba0ad24951b09a68acd030f85b
Heriberto Basulto <basulto93@live.com>
2016-09-20 11:37:09 -0700

    Menu state displays all menu items, ability to add and delete items as well

--- app/js/menu/menu.controller.js
+++ app/js/menu/menu.controller.js
@@ -5,11 +5,48 @@
         .module('app')
         .controller('RestaurantMenuController', RestaurantMenuController);
 
-    RestaurantMenuController.$inject = [];
+    RestaurantMenuController.$inject = ['MenuGroupFactory', 'MenuItemFactory', '$stateParams', 'RestaurantFactory'];
 
-    /* @ngInject */
-    function RestaurantMenuController() {
+    function RestaurantMenuController(MenuGroupFactory, MenuItemFactory, $stateParams, RestaurantFactory) {
         var vm = this;
+
+        vm.menu = [];
+        vm.deleteMenuItem = deleteMenuItem;
+        vm.addMenuItem = addMenuItem;
+        getMenu();
+
+/******* GET ALL ITEMS ON THE MENU ***************************************************************************************/
+        function getMenu(){
+        	RestaurantFactory.getById($stateParams.restaurantId).then(
+        		function(response){
+        			vm.menu = response.menuGroups;
+        			console.log(vm.menu);
+        		}
+        	);
+        }
+/******** DELETE AN ITEM **************************************************************************************/
+        function deleteMenuItem(menuItem){
+        	if(confirm("Are you sure you want to remove this item?")){
+	        	MenuItemFactory.remove(menuItem.menuItemId).then(
+	        		function(response){
+	        			getMenu();
+	        			alert("Item Deleted");
+	        		}
+	        	);	
+        	}
+        }
+/********** ADD AN ITEM ************************************************************************************/
+        function addMenuItem(newMenuItem, menuGroupId){
+        	newMenuItem.menuGroupId = menuGroupId;
+        	MenuItemFactory.add(newMenuItem).then(
+        		function(){
+        			getMenu();
+        			newMenuItem = {};
+        		}
+        	);
+        }
         
     }
 })();
+
+
